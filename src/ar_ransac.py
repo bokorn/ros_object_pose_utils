a873295439ba775da533e99cdaf3e54381dc4da0
Brian Okorn <brian.e.okorn@gmail.com>
2019-02-17 21:45:31 -0500

    Changed ransac to contain corneres.

--- src/ar_ransac.py
+++ src/ar_ransac.py
@@ -14,6 +14,7 @@ def parseBundleXml(filename):
     tree = ET.parse(filename)
     root = tree.getroot()
     centers = {}
+    corners = {}
     min_pts = []
     max_pts = []
     for marker in root: 
@@ -23,27 +24,33 @@ def parseBundleXml(filename):
             corner_pts.append([float(corner.get('x'))/100.0, 
                                float(corner.get('y'))/100.0, 
                                float(corner.get('z'))/100.0]) 
+        corners[idx] = corner_pts
         centers[idx] = np.mean(corner_pts, axis=0) 
         min_pts.append(np.min(corner_pts, axis=0)) 
         max_pts.append(np.max(corner_pts, axis=0)) 
         interior = (np.max(min_pts, axis=0), np.min(max_pts, axis=0))
-    return centers, interior
+    return corners, centers, interior
 
 class ARTagRANSAC(object):
     def __init__(self,
                  ):
         rospy.init_node("ar_tag_ransac")
         self.frame_id = rospy.get_param('~output_frame', 'marker_bundle')
+        tag_width = rospy.get_param('~tag_width', 0.06)
         rospack = rospkg.RosPack()
         self.bundle_file = rospy.get_param('~bundle_file', 
                 os.path.join(rospack.get_path('object_pose_utils'), 'bundles/training_all_mat.xml'))
-        self.max_corres_dist = rospy.get_param('~max_distance', 0.01)
+        self.max_corres_dist = rospy.get_param('~max_distance', 0.005)
         self.tf_broadcaster = tf.TransformBroadcaster()
         
-        self.marker_centers, self.marker_interior = parseBundleXml(self.bundle_file) 
+        self.marker_corners, self.marker_centers, self.marker_interior = parseBundleXml(self.bundle_file) 
         self.marker_ids = list(self.marker_centers.keys())
-        self.marker_sub = rospy.Subscriber('in_markers', AlvarMarkers, self.markersCallback, queue_size = 1) 
-
+        self.marker_sub = rospy.Subscriber('/ar_pose_marker', AlvarMarkers, self.markersCallback, queue_size = 1) 
+        #self.marker_sub = rospy.Subscriber('in_markers', AlvarMarkers, self.markersCallback, queue_size = 1) 
+        self.tag_corners = np.array([[-tag_width/2.0,-tag_width/2.0, 0.0, 1.0], 
+                                     [ tag_width/2.0,-tag_width/2.0, 0.0, 1.0],
+                                     [ tag_width/2.0, tag_width/2.0, 0.0, 1.0],
+                                     [-tag_width/2.0, tag_width/2.0, 0.0, 1.0]])
     def markersCallback(self, markers_msg):
         stamp = markers_msg.header.stamp
         confidence = []
@@ -51,15 +58,18 @@ class ARTagRANSAC(object):
         bundle_pts = []
         for marker in markers_msg.markers:
             if(marker.id in self.marker_centers.keys()):
-                #q = marker.pose.pose.orientation
+                q = marker.pose.pose.orientation
                 pt = marker.pose.pose.position
-                #trans_mat = quaternion_matrix([q.x,q.y,q.z,q.w])
-                #trans_mat[:3,3] = [pt.x, pt.y, pt.z]
+                trans_mat = tf.transformations.quaternion_matrix([q.x,q.y,q.z,q.w])
+                trans_mat[:3,3] = [pt.x, pt.y, pt.z]
+                corners = self.tag_corners.dot(trans_mat.T)
                 #marker_poses[marker.id] = trans_mat
+                #confidence.append(marker.confidence)
                 marker_pts.append([pt.x, pt.y, pt.z])
-                confidence.append(marker.confidence)
                 bundle_pts.append(list(self.marker_centers[marker.id]))
-
+                marker_pts.extend(corners[:,:3])
+                bundle_pts.extend(list(self.marker_corners[marker.id]))
+         
         pts_src = open3d.PointCloud()
         pts_src.points = open3d.Vector3dVector(marker_pts) 
         pts_tgt = open3d.PointCloud()
