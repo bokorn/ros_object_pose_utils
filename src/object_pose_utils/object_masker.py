adaad065982c6444ca7f743a0b3624a9ebfec5ed
Philip Strawser <philip.a.strawser@nasa.gov>
2019-02-27 13:50:44 -0600

    added comments

--- src/object_pose_utils/object_masker.py
+++ src/object_pose_utils/object_masker.py
@@ -60,8 +60,28 @@ def getCMapImage(image, cmap='gray'):
     img_cmap = plt.imshow(image, cmap=cmap)._rgba_cache
     return img_cmap
 
-def centerSortX(image, anns, num_categories = np.inf):
 
+def centerSortX(image, anns, num_categories = np.inf):
+    """
+
+    Args:
+        image: input image
+        anns: dictionary of mask index in the masked image to a dictionary of imantics.bbox and imantics.mask
+            ann : {
+                int : {
+                    "bbox" :
+                    "mask":
+                    },
+                int : {
+                    "bbox" :
+                    "mask":
+                    },
+                ...
+        num_categories:
+
+    Returns:
+        Dictionary of mask index(int) to category(int)
+    """
     if(num_categories < len(anns)):
         anns_prunned = {}
         areas = [v['bbox'].area for _,v in anns.items()]
@@ -78,6 +98,7 @@ def centerSortX(image, anns, num_categories = np.inf):
  
     return dict(zip(np.array(anns.keys())[np.argsort(centers)], range(len(anns))))
 
+
 class ObjectMasker(object):
     def __init__(self, thresh_block_size, thresh_const, image_roi=None):
         """
@@ -87,8 +108,6 @@ class ObjectMasker(object):
             thresh_const: constant used in adaptive thresholding
             image_roi: region of interest in [x1, y1, x2, y2] format
         """
-        rospy.init_node("object_masker")
-
         self.image_roi = image_roi
         self.thresh_block_size = thresh_block_size
         self.thresh_constant = thresh_const
@@ -105,11 +124,15 @@ class ObjectMasker(object):
     def getMasks(self, image, roi_mask = None):
         """ Get objects masks from the image
 
+        The output masks are stored in a single "image" as an ndarray of connected components.  It has the same width &
+        height as the input image where the value of each "pixel" corresponds to a connected component.  Pixels with the
+        same value are considered to be in the same component.
+
         Args:
             image: input BGR image as an ndarray
 
         Returns:
-            List of masks, where each mask is an opencv contour
+            Tuple of a mask "image" and list of corresponding filter IDs.
         """
         # create a mask for the ROI
         if(roi_mask is None):
@@ -126,7 +149,7 @@ class ObjectMasker(object):
         all_markers = segmentImage(image, self.thresh_block_size, self.thresh_constant)
         markers_masked = (all_markers + 1) * roi_mask
 
-        # Filter out really small and reall large components (noise and background)
+        # Filter out really small and really large components (noise and background)
         filtered_idxs, filtered_counts = filterMarkers(markers_masked, roi_size / 250, roi_size / 3)
 
         # Set markers that are outside our filter box to zero
@@ -137,6 +160,20 @@ class ObjectMasker(object):
     def getAnnotations(self, image, masks, mask_ids = None, 
                        category_names = None, category_colors = None, 
                        category_func = None):
+        """
+
+        Args:
+            image: input BGR image as and ndarray
+            masks: connected component "mask" image from getMasks as an ndarray
+            mask_ids: list of component values
+            category_names: list of strings
+            category_colors:
+            category_func:
+
+        Returns:
+
+        """
+
         ann_img = imantics.Image(image_array = image)
          
         if(mask_ids is None):
