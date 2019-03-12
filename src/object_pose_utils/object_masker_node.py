1f3821fb51dd43284dfd5f66e2f548527c14877f
Philip Strawser <philip.a.strawser@nasa.gov>
2019-03-12 13:55:08 -0500

    added "object_select" parameter to object_masker_node

--- src/object_pose_utils/object_masker_node.py
+++ src/object_pose_utils/object_masker_node.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from ast import literal_eval
 from multiprocessing import Lock
+import os
 
 import numpy as np
 import cv2
@@ -11,11 +12,11 @@ from sensor_msgs.msg import Image, CameraInfo
 from object_pose_msgs.msg import LabeledComponents
 import message_filters
 
-from functools import partial
 from object_masker import ObjectMasker
 from feature_classification import FeatureClassifier, ClassificationMapper, cropBBox
 
 roslib.load_manifest("rosparam")
+
 
 class ObjectMaskerNode(object):
 
@@ -31,7 +32,7 @@ class ObjectMaskerNode(object):
 
         self.info_mutex = Lock()
         self.bridge = CvBridge()
-        
+
         self.image_sub = message_filters.Subscriber('in_image', Image)
         self.info_sub = message_filters.Subscriber('in_camera_info', CameraInfo)
         self.image_pub = rospy.Publisher('out_image', Image, queue_size = 1)
@@ -42,10 +43,12 @@ class ObjectMaskerNode(object):
         self.classifier = FeatureClassifier(visual_dict)
         self.mapper = ClassificationMapper(self.classifier)
         self.categories = self.classifier.class_names
-        self.obj_idxs = {k:0 for k in self.classifier.class_names.keys()} 
-        
+        self.obj_idxs = {k: 0 for k in self.classifier.class_names.keys()}
+
+        self.object_select = rospy.get_param('~object_select', default=None)
         self.output_folder = rospy.get_param('~output_folder', default=None)
-        if(self.output_folder is not None):
+
+        if self.output_folder is not None:
             for cat_name in self.category_names:
                 if not os.path.exists(os.path.join(self.output_folder, cat_name)):
                     os.makedirs(os.path.join(self.output_folder, cat_name))
@@ -61,22 +64,42 @@ class ObjectMaskerNode(object):
             rospy.logerr(err)
             return
 
+        rospy.loginfo("looking for object:{}".format(self.object_select))
+
         masks, mask_idxs = self.masker.getMasks(img)
         ann_img, category_map = self.masker.getAnnotations(img, masks, mask_idxs,categories=self.categories, mapper=self.mapper)
-
 
         labeled_components_msg = LabeledComponents()
          
         mask_img = np.zeros_like(masks, dtype=np.uint8)
+        name_to_label = {}
+        output_object = None
+
+        print "self.object_select = {}".format(self.object_select)
+        if self.object_select is not None:
+            if self.object_select in self.categories.values():
+                output_object = self.object_select
+
+        print self.categories
+        # build the mask image and the labeled components
         for mask_id, cat_id in sorted(category_map.items(), key=lambda x: x[1]):
             cat_id = np.uint8(cat_id)
-            mask_img[masks==mask_id] = cat_id
+
+            print "cat_id:{} self.categories[cat_id]:{} output_object={}".format(cat_id, self.categories[cat_id], output_object)
+            # if there is a valid output object name, set the mask to 1.  This allows for a single object mask
+            if self.categories[cat_id] == output_object:
+                rospy.loginfo('matched on {} {}'.format(cat_id, output_object))
+                mask_img[masks == mask_id] = 1
+            # else, set the mask to the category id
+            if self.object_select is None:
+                mask_img[masks == mask_id] = cat_id
             labeled_components_msg.names.append(self.categories[cat_id])
             labeled_components_msg.labels.append(cat_id)
+            name_to_label[self.categories[cat_id]] = cat_id
         
         display_img = ann_img.draw(thickness=1, color_by_category=True)
        
-        if(self.output_folder is not None):
+        if self.output_folder is not None:
             for ann in ann_img.annotations.values():
                 img_crop = cropBBox(img, ann.mask.bbox())
                 mask_crop = cropBBox(ann.mask.array, ann.mask.bbox())
@@ -92,13 +115,12 @@ class ObjectMaskerNode(object):
             display_msg = self.bridge.cv2_to_imgmsg(display_img.astype(np.uint8), encoding="bgr8")
             mask_msg = self.bridge.cv2_to_imgmsg(cv2.normalize(mask_img, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX), encoding="mono8")
             labeled_components_msg.image = self.bridge.cv2_to_imgmsg(mask_img)
-
-
         except CvBridgeError as err:
             rospy.logerr(err)
             return
 
         display_msg.header = img_msg.header
+        mask_msg.header = img_msg.header
         self.image_pub.publish(display_msg)
         self.mask_pub.publish(mask_msg)
         self.labeled_components_pub.publish(labeled_components_msg)
